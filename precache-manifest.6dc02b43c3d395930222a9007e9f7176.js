dcf3da6fb7c68b57468532279cbed1f01e56fa48
troyoram <oram0720@gmail.com>
2018-11-20 16:14:27 -0500

    Updates

---
+++ precache-manifest.6dc02b43c3d395930222a9007e9f7176.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c9c5871ac0eaadc3a8d",
+    "url": "/got/static/js/runtime~main.3c9c5871.js"
+  },
+  {
+    "revision": "a3e78c67c8468689f1d7",
+    "url": "/got/static/js/main.a3e78c67.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/got/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "a3e78c67c8468689f1d7",
+    "url": "/got/static/css/main.017867f8.chunk.css"
+  },
+  {
+    "revision": "67015e085b45929bf536eb3a28d734ce",
+    "url": "/got/index.html"
+  }
+];
